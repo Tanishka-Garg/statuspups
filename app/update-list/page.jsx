9f9aaa44f35be82b6a9769a0829a60c4d0ebc8f6
Tanishka-Garg <tan27peace@gmail.com>
2024-08-10 15:25:39 +0530

    .

--- app/update-list/page.jsx
+++ app/update-list/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Form from "@components/Form";
 
@@ -61,4 +61,10 @@ const UpdateList = () => {
   );
 };
 
-export default UpdateList;
+export default function UpdateListPage() {
+  return (
+    <Suspense>
+      <UpdateList></UpdateList>
+    </Suspense>
+  );
+}
